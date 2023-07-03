fd749eeeb3f59d8c2e083a85dbad312b243edde6
GabGou <gabrielriberikoa@gmail.com>
2023-07-03 12:12:03 -0300

    Variáveis

---
+++ aula030723/variaveis.js
@@ -0,0 +1,26 @@
+/*
+   Java - Linguagem de Programação (Compilada)
+   JavaScript - Linguagem de Programação (Interpretada)
+
+   Chrome -> V8 -> Interpretador do JS
+   Node.js -> V8 + NPM (Node Package Manager)
+*/
+
+console.log("Hello World") //Imprime informações no terminal
+
+// Variáveis -> Espaço (na memória ram) para armazenar um valor
+let nome; // Declaração de variável
+nome = "Gabriel Riberi Koa";
+
+let idade = 20;
+let eCasado = false;
+
+console.log(nome + " eu tenho " + idade + ", e se eu sou casado? " + eCasado);
+
+nome = "Daniel Cardoso";
+idade = 34;
+eCasado = true;
+console.log(nome + " eu tenho " + idade + ", e se eu sou casado? " + eCasado);
+
+const PI = 3.14;
+console.log(PI);
