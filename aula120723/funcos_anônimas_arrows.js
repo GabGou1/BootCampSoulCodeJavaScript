f53f6b4d157067278db5b8f63d83b3342e74160b
GabGou1 <gabrielriberikoa@gmail.com>
2023-07-12 13:42:29 -0300

    Aula do dia 12/07/23

--- aula120723/funcos_anônimas_arrows.js
+++ aula120723/funcos_anônimas_arrows.js
@@ -44,18 +44,47 @@
 // setInterval(teste, 2000)
 
 // A função setTimeout será executada apenas uma única vez. Caso deseje que a execução aconteça várias vezes, então você poderá usar a função setInterval
-setInterval(function () {
-    console.log("Olá Mundo!");
-}, 2000);
+// setInterval(function () {
+//     console.log("Olá Mundo!");
+// }, 2000);
 
 //Arrow function
 
-() => {
-    console.log("Olá Mundo! Com timeout e arrowFunction");
-}
+// () => {
+//     console.log("Olá Mundo! Com timeout e arrowFunction");
+// }
 
 //parametros => escopo/instrução
-(resultado, requisição) => {
-    console.log(resultado + " " + requisição)
-    return 0;
+// (resultado, requisição) => {
+//     console.log(resultado + " " + requisição)
+//     return 0;
+// }
+
+// Testando a utilidade de arrow function
+function teste(callback) {
+    let valor = callback(130);
+    console.log(valor);
 }
+
+// Primeira forma de utilizar teste
+function vezes2(numero) {
+    return (numero * 2);
+}
+teste(vezes2);
+
+// Segunda forma de utilizar teste
+teste(function(numero){
+    return numero * 2;
+})
+
+// Terceira forma de utilizar teste (Função Arrow) - Modo 1
+teste((numero) => {
+    return (numero * 2);
+});
+
+// Quarta forma de utilizar teste (Função Arrow) - Modo 2
+teste(numero => (numero * 2));
+
+setTimeout(_ => console.log("Olá, Mundo!"), 3000)
+//Timeout -> quando o tempo passa, ele printa
+//Interval -> printa de tempos em tempos
